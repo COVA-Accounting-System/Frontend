4c9f83c0bec65b0f56da5f895dcdcaf2ac6bbb2b
jacovzap <jacovzap@gmail.com>
2022-10-07 00:17:20 -0400

    client crud complete

--- src/components/DialogModal/ViewModal/ViewForm.js
+++ src/components/DialogModal/ViewModal/ViewForm.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 import "../DialogModal.scss";
 
 const ViewForm = () => {
+  const client = useSelector((status) => status.clients.actualClient);
+  const entity = useSelector((status) => status.crud.entityName);
+  const action = useSelector((status) => status.crud.action);
   return (
-    <div>
-        <h1> Ver </h1>
+    <div className="view-form">
+      <h1 className="title-container"> Ver {entity}</h1>
+      <hr className="view-hr" />
+      <div className="data-container">
+        <div className="row-data-container">
+          <p className="row-data">
+            {/* <strong>Nombre: </strong> */}
+            {client.name} {client.lastName}
+          </p>
+        </div>
+        <div className="row-data-container">
+          
+          <p className="row-data">
+            {/* <strong>Telfono: </strong>  */}
+            {client.phone} </p>
+        </div>
+        <div className="row-data-container">
+          <p className="row-data">
+            <strong>Deuda:  </strong>
+            {client.inDebt}
+          </p>
+        </div>
+      </div>
+      <div className="form-footer"></div>
     </div>
-  )
-}
+  );
+};
 
-export default ViewForm
+export default ViewForm;
